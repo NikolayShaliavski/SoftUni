8276821900856776e542c570fbacc8c638904ffc
Nikolay Shaliavski <nshaliavski@gmail.com>
2016-08-20 02:54:42 +0300

    Recursion exercises from Algorithms course April 2016 - code C#

---
+++ My_Practice_Algorithms_April_2016/01.RecursionExercise/01.RecursiveArraySum/RecursiveArraySum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.RecursiveArraySum
+{
+    class RecursiveArraySum
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4 };
+            int index = int.Parse(Console.ReadLine());
+
+            if (index >= numbers.Length)
+            {
+                Console.WriteLine("Index is out of bounds of the array!");
+                return;
+            }
+            long result = FindSum(numbers, index);
+            Console.WriteLine(result);
+        }
+
+        private static long FindSum(int[] arr, int index)
+        {
+            if (index == arr.Length - 1)
+            {
+                return arr[index];
+            }
+            return arr[index] + FindSum(arr, index + 1);
+        }
+    }
+}
