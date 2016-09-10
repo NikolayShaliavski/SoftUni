110a2c17cff4f0e5bebd97cbf0e05b1aed4ad135
Nikolay Shaliavski <nshaliavski@gmail.com>
2016-09-10 21:09:36 +0300

    DFS && BFS Algorithms

--- My_Practice_Algorithms_April_2016/04.Dynamic_Programming_Homework/05.SumUnlimitedAmountOfCoins/SumUnlimitedAmountOfCoins.cs
+++ My_Practice_Algorithms_April_2016/04.Dynamic_Programming_Homework/05.SumUnlimitedAmountOfCoins/SumUnlimitedAmountOfCoins.cs
@@ -1,25 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _05.SumUnlimitedAmountOfCoins
 {
     class SumUnlimitedAmountOfCoins
     {
-        private static int allSums;
+        private static int allSumsCount;
         private static int sum;
+        private static List<int> currentCom;
+        private static List<string> allSums;
 
         static void Main(string[] args)
         {
-            var targetSum = 5;
-            int[] coins = { 1, 2, 5 };
-            allSums = 0;
+            var targetSum = 100;
+            int[] coins = { 1, 2, 5, 10, 20, 50, 100 };
+            allSumsCount = 0;
             sum = 0;
+            currentCom = new List<int>();
+            allSums = new List<string>();
 
             GenSum(targetSum, coins);
-            Console.WriteLine(allSums);
+            Console.WriteLine(allSumsCount);
+
+            //foreach (var sum in allSums)
+            //{
+            //    Console.WriteLine(sum);
+            //}
         }
 
         private static void GenSum(int targetSum, int[] coins)
@@ -30,15 +36,22 @@ namespace _05.SumUnlimitedAmountOfCoins
             }
             if (sum == targetSum)
             {
-                allSums++;
-                //sum = 0;
+                currentCom.Sort();
+                string sum = string.Join("", currentCom);
+                if (!allSums.Contains(sum))
+                {
+                    allSumsCount++;
+                    allSums.Add(sum);
+                }
                 return;
             }
             for (int i = 0; i < coins.Length; i++)
             {
                 sum += coins[i];
+                currentCom.Add(coins[i]);
                 GenSum(targetSum, coins);
                 sum -= coins[i];
+                currentCom.Remove(coins[i]);
             }
         }
     }
