3f1cd4bfbe2ec3e4af4297ae22cd2dc119b2462f
Nikolay Shaliavski <nshaliavski@gmail.com>
2016-10-26 00:31:01 +0300

    DFS && BFS Lab.

---
+++ My_Practice_Data_Structures/05.Tree_And_Graph_Traversal_Algorithms_LAB/DFS-Graph-Traversal/GraphConnectedComponents.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GraphConnectedComponents
+{
+    private static List<int>[] graph;
+    private static bool[] visited;
+
+    public static void Main()
+    {
+        ReadGraph();
+
+        FindGraphConnectedComponents();
+    }
+
+    private static void ReadGraph()
+    {
+        var n = int.Parse(Console.ReadLine());
+        graph = new List<int>[n];
+        visited = new bool[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            graph[i] = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).
+                Select(int.Parse).ToList();
+        }
+    }
+
+    private static void FindGraphConnectedComponents()
+    {
+        for (int startNode = 0; startNode < graph.Length; startNode++)
+        {
+            if (!visited[startNode])
+            {
+                Console.Write("Connected component:");
+                Dfs(startNode);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    private static void Dfs(int node)
+    {
+        if (!visited[node])
+        {
+            visited[node] = true;
+            foreach (var child in graph[node])
+            {
+                Dfs(child);
+            }
+            Console.Write(" " + node);
+        }
+    }
+}
