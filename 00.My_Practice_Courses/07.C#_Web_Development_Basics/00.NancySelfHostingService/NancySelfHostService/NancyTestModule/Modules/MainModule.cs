d8a9111a88eb57dcd7e8e34876773b38824eb1ba
Nikolay Shalyavski <nikolay.shalyavski@atia.com>
2019-02-25 13:11:09 +0200

    Added Owin self hosting project. Can be used instead of Nancy self-hosting, supports concurrent (parallel) requests.

--- 00.My_Practice_Courses/07.C#_Web_Development_Basics/00.NancySelfHostingService/NancySelfHostService/NancyTestModule/Modules/MainModule.cs
+++ 00.My_Practice_Courses/07.C#_Web_Development_Basics/00.NancySelfHostingService/NancySelfHostService/NancyTestModule/Modules/MainModule.cs
@@ -26,10 +26,33 @@ namespace NancyTestModule.Modules
                 }
                 return Response.AsJson(fileInfo);
             };
+            Post["/fileinfoasync", runAsync: true] = async (x, ct) =>
+            {
+                FileInfoRequest req = this.Bind<FileInfoRequest>();
+                FileInfoResponse fileInfo = await this.GetFileInfoAsync(req.Path);
+                if (fileInfo == null)
+                {
+                    return 404;
+                }
+                return Response.AsJson(fileInfo);
+            };
         }
         private FileInfoResponse GetFileInfo(string fileName)
         {
-            Console.WriteLine($"[REQUEST][{DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}] File: {fileName}");
+            Console.WriteLine($"[REQUEST GetFileInfo sync][{DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}] File: {fileName}");
+            FileInfoResponse res = null;
+            if (!File.Exists(fileName))
+            {
+                return res;
+            }
+            FileInfo file = new FileInfo(fileName);
+            res = new FileInfoResponse(file.FullName, file.Length, file.CreationTime, file.LastWriteTime);
+            return res;
+        }
+        private async Task<FileInfoResponse> GetFileInfoAsync(string fileName)
+        {
+            await Task.Delay(5000);
+            Console.WriteLine($"[REQUEST GetFileInfo async][{DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}] File: {fileName}");
             FileInfoResponse res = null;
             if (!File.Exists(fileName))
             {
---
+++ 00.My_Practice_Courses/07.C#_Web_Development_Basics/00.NancySelfHostingService/NancySelfHostService/OwinSelfHost/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Configuration.Install;
+using System.Reflection;
+using System.ServiceProcess;
+
+namespace OwinSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                //Install service
+                if (args[0].Trim().ToLower() == "/i")
+                {
+                    ManagedInstallerClass.InstallHelper(new string[] {
+                        "/i", Assembly.GetExecutingAssembly().Location
+                    });
+                }
+                //Uninstall service                 
+                else if (args[0].Trim().ToLower() == "/u")
+                {
+                    ManagedInstallerClass.InstallHelper(new string[] {
+                        "/u", Assembly.GetExecutingAssembly().Location
+                    });
+                }
+                return;
+            }
+            if (Environment.UserInteractive)
+            {
+                OwinService owinService = new OwinService();
+                owinService.TestAsConsoleApp(args);
+                return;
+            }
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new OwinService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
