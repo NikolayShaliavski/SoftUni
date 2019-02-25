2440c1ff23272f7990d56438aaa352cbc84acfaa
Nikolay Shalyavski <nikolay.shalyavski@atia.com>
2019-02-25 14:02:24 +0200

    Added Owin.Limits dependency to OwinSelfHost project.

--- 00.My_Practice_Courses/07.C#_Web_Development_Basics/00.NancySelfHostingService/NancySelfHostService/OwinSelfHost/Startup.cs
+++ 00.My_Practice_Courses/07.C#_Web_Development_Basics/00.NancySelfHostingService/NancySelfHostService/OwinSelfHost/Startup.cs
@@ -6,6 +6,7 @@ namespace OwinSelfHost
     {
         public void Configuration(IAppBuilder app)
         {
+            app.MaxConcurrentRequests(1000);
             app.UseNancy();
         }
     }
